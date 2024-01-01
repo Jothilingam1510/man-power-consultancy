6b7fe0a9f88c1cbd10e69db75ffd94ae123ce9b7
Jothilingam1510 <136618756+Jothilingam1510@users.noreply.github.com>
2024-01-01 21:07:45 +0530

    add short name

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,27 +1,49 @@
-import React, { useState } from 'react'
-import './Nav.css'
-import { Link } from 'react-router-dom'
-import {FaBars,FaTimes} from 'react-icons/fa'
-const Nav = () => {
-  const [click,setClick]=useState(false)
-  const handleClick=()=>setClick(!click)
+import React, { useState, useEffect } from 'react';
+import './Nav.css';
+import { Link } from 'react-router-dom';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
-  const [color,setColor]=useState(false)
-  const changeColor =()=>{
-    if(window.scrollY >=100){
-      setColor(true)
-    }else{
-      setColor(false)
+const Nav = () => {
+  const [click, setClick] = useState(false);
+  const [color, setColor] = useState(false);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  const handleClick = () => setClick(!click);
+
+  const changeColor = () => {
+    if (window.scrollY >= 100) {
+      setColor(true);
+    } else {
+      setColor(false);
     }
   };
-  window.addEventListener("scroll",changeColor);
+
+  const handleResize = () => {
+    setScreenWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', changeColor);
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('scroll', changeColor);
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []); // Adding empty dependency array to run the effect only once on mount
+
+  // Condition to change the text based on screen width
+  const headerText = screenWidth <= 600 ? 'AM CONSULTANCY' : 'ALAGAR MALAIYAN CONSULTANCY';
+
   return (
     <>
-    <div className={color ? "header header-bg" : "header"}>
+      <div className={color ? 'header header-bg' : 'header'}>
         <Link to="/">
-            <p className=' jo'><span>ALAGAR </span>MALAIYAN CONSULTANCY</p>
+          <p className='jo'>
+            <span>{headerText}</span>
+          </p>
         </Link>
-        <ul className={click ? "nav-menu active" : "nav-menu"}>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
           <li>
             <Link to='/'>Home</Link>
           </li>
@@ -37,14 +59,17 @@ const Nav = () => {
           <li>
             <Link to='/Getintouch'>Get in touch</Link>
           </li>
-        </ul>  
+        </ul>
         <div className='hamburger' onClick={handleClick}>
-        { click ? (<FaTimes size={25} style={{color:"white"}}/>) : (<FaBars size={25} style={{color:"white"}}/>)}
-
+          {click ? (
+            <FaTimes size={25} style={{ color: 'white' }} />
+          ) : (
+            <FaBars size={25} style={{ color: 'white' }} />
+          )}
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
